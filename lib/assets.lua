f5a24ed124a4abd8f843160ad0709e600950e00f
SkyVault <dustinneumann42@gmail.com>
2022-10-22 16:09:45 -0700

    image sets

--- lib/assets.lua
+++ lib/assets.lua
@@ -1,5 +1,6 @@
 return function(love)
   local images = {}
+  local image_sets = {}
 
   local function get_image(id)
     -- TODO: Return a demo texture if not found
@@ -12,8 +13,30 @@ return function(love)
     return img
   end
 
+  local function image_set(set_id, ...)
+    local res = {...}
+    image_sets[set_id] = res
+    return res
+  end
+
+  local function get_images_from_set(set_id)
+    local res = {}
+    local iset = image_sets[set_id]
+    for i = 1, #iset do
+      res[i] = get_image(iset[i])
+    end
+    return res
+  end
+
+  local function get_image_set(set_id)
+    return image_sets[set_id]
+  end
+
   return {
     get_image = get_image,
     load_image = load_image,
+    image_set = image_set,
+    get_images_from_set = get_images_from_set,
+    get_image_set = get_image_set,
   }
 end
